6f62d6485bf1f8db7588f3b1a05193169bb143ed
Lauri Taskinen <lauri.taskinen417@gmail.com>
2021-05-15 15:54:29 +0300

    routing fix

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.css']
+  styleUrls: ['./booking.component.css'],
 })
 export class BookingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
