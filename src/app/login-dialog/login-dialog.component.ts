c4559959671ae86692c5b13937132428fa58cfb9
Eeva Takanen <eevakristiinatakanen@gmail.com>
2021-05-18 20:16:03 +0300

    small fixes

--- src/app/login-dialog/login-dialog.component.ts
+++ src/app/login-dialog/login-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login-dialog',
@@ -9,23 +10,27 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-dialog.component.css'],
 })
 export class LoginDialogComponent implements OnInit {
-
-  constructor(private router: Router, private auth: AuthService,) {}
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {}
 
-  signIn() {
+  signIn(): void {
     this.router.navigate(['/sign-in']);
+    this.dialog.closeAll();
   }
 
-  logIn(data: NgForm) {
+  logIn(data: NgForm): void {
     if (this.auth.loggedIn) {
       console.log(data.value);
       data.resetForm();
       this.router.navigate(['/booking']);
+      this.dialog.closeAll();
     } else {
       throw 'Sisäänkirjautuminen epäonnistui';
     }
   }
-
 }
